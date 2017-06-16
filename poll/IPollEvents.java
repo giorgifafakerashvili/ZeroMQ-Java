012832379f2617a4979a802f2262e997a3b38fb8
= <giorgi.fafa@gmail.com>
2017-06-17 02:34:16 +0400

    added IPollEvents.java interface

---
+++ poll/IPollEvents.java
@@ -0,0 +1,18 @@
+public interface IPollEvents {
+
+  // Called by I/O thread when file descriptor is ready for reading
+  void inEvent();
+
+  // Calle by I/O thread when file descriptor is ready for writing
+  void outEvent();
+
+  // Called by I/O thread when file descriptor might be ready for connecting
+  void connectEvent();
+
+  // Called by I/O thread when file descriptor is ready for accept
+  void acceptEvent();
+
+  // Called when timer expires
+  void timerEvent(int id);
+  
+}
